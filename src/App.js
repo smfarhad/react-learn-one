a5001686489be64e79623de2a4025f2c589d40d1
smfarhad <farhad1556@gmail.com>
2021-08-14 09:16:53 +0600

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Farhad",
+      course: "React from Zero to Hero"
+    }
+  }
+  changeStateData = () => {
+    this.setState(
+      {
+        userName: this.state.userName === "Farhad" ? "Hossain" : "Farhad"
+      }
+    )
+  }
+  render() {
+    return (
+      <div>
+        <h4 className="bg-primary text-white text-center p-2">
+          {this.state.userName} React Course "{this.state.course}"
+        </h4>
+        <button className="btn btn-warning m-2" onClick={this.changeStateData}>Change</button>
+    </div> 
+    );
+  }
+};
